131f8bccd24e40a59750d310a4c0b6b68baeeee2
charlesndalton <charlesndalton@gmail.com>
2022-05-11 11:23:11 -0700

    ops: changes

---
+++ tests/test_dummy.py
@@ -0,0 +1,6 @@
+
+def test_operation(
+    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX,
+    tokemak_manager, tokemak_eth_pool, account_with_tokemak_rollover_role, utils
+):
+    assert False
